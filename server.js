b922f5e47e7d90cdda05d1a9ec11bd8fc7388da2
Isaac Dennis <idennisgsu3@gmail.com>
2020-08-20 13:54:58 -0400

    Got local sever working

---
+++ server.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+
+// dependencies
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
+// CONSTANTS
+const PORT = process.env.PORT || 3000;
+
+// middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+app.use(methodOverride('_method'));
+const mongoURI = process.env.MONGO_URI
+// mongoose connection
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', () => {
+    console.log("connected to mongo");
+})
+
+// Controller
+
+
+
+// Default route:
+app.get("/", (req, res) => {
+    res.send('Hello World')
+  });
+  
+// listen
+app.listen(PORT, () => {
+    console.log('listening on: ' + PORT);
+});
