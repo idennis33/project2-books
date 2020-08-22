65a9c4dfc43f2b22f98652c14a4164632bf855e6
Isaac Dennis <idennisgsu3@gmail.com>
2020-08-22 16:53:57 -0400

    Added some styling to my css page

--- views/components/Default.jsx
+++ views/components/Default.jsx
@@ -33,10 +33,6 @@ class Default extends React.Component {
                         <a className="nav-link" href={`/wishs`}>Wish List</a>
                     </li>
                 </ul>
-                <form className="form-inline my-2 my-lg-0">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
             </div>
         </nav>    
             {this.props.children}
